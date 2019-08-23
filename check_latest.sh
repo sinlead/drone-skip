173e0369047766cc198c0a59254630c3d8caabf6
David Liu <msglight4874@gmail.com>
2019-08-23 14:03:06 +0800

    Remove `\n` in log message

--- check_latest.sh
+++ check_latest.sh
@@ -5,21 +5,21 @@ echo "machine ${CI_NETRC_MACHINE}" > "${HOME}/.netrc"
 echo "login ${CI_NETRC_USERNAME}" >> "${HOME}/.netrc"
 echo "password ${CI_NETRC_PASSWORD}" >> "${HOME}/.netrc"
 
-echo "Initialize temporary repository\n"
+echo "Initialize temporary repository"
 mkdir -p /tmp/drone-skip/
 cd /tmp/drone-skip/
 git init
 git remote add origin $DRONE_REMOTE_URL
 
-echo "Fetch commit SHA of remote origin\n"
+echo "Fetch commit SHA of remote origin"
 ORIGIN_SHA=$(git ls-remote origin | grep "refs/heads/${DRONE_SOURCE_BRANCH}$" | awk '{print $1}')
 echo "Commit SHA of remote origin: ${ORIGIN_SHA}"
-echo "Commit SHA of this build: ${DRONE_COMMIT_SHA}\n"
+echo "Commit SHA of this build: ${DRONE_COMMIT_SHA}"
 
-echo "Remote temporary repositiory\n"
+echo "Remote temporary repositiory"
 rm -rf /tmp/drone-skip/
 
-echo "Check whether commit SHA of this build is latest SHA\n"
+echo "Check whether commit SHA of this build is latest SHA"
 if [ "${DRONE_COMMIT_SHA}" = "${ORIGIN_SHA}" ]; then 
   echo "It's latest build. Contiune..."
   exit 0
